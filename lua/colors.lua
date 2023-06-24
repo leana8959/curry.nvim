7dca1c3d3a9b394c7c9805f73b3d0883050e1dc5
Léana 江 <leana.jiang@icloud.com>
2023-06-24 11:38:36 +0200

    mod(colors): better diff colors

--- lua/colors.lua
+++ lua/colors.lua
@@ -10,34 +10,38 @@ if vim.o.background == "dark" then
          @syntax-gutter-background-color-selected: lighten(@syntax-bg, 8%);
          for color in $(cat colors/one-nvim.vim | head -n 41 | tail -n 28 | cut -d '#' -f2 | cut -d '"' -f1); do hex2xterm $color | grep 'Hexadecimal\|xterm256'; done
     --]]
-	mono_1                = { "#abb2bf", 145, "mono_1" }     -- hsl(@syntax-hue, 14%, 71%);
-	mono_2                = { "#828997", 102, "mono_2" }     -- hsl(@syntax-hue,  9%, 55%);
-	mono_3                = { "#5c6370", 59, "mono_3" }      -- hsl(@syntax-hue, 10%, 40%);
-	mono_4                = { "#4b5263", 240, "mono_4" }     --
-	hue_1                 = { "#56b6c2", 247, "hue_1" }      -- hsl(187, 47%, 55%);
-	hue_2                 = { "#61afef", 75, "hue_2" }       -- hsl(207, 82%, 66%);
-	hue_3                 = { "#c678dd", 176, "hue_3" }      -- hsl(286, 60%, 67%);
-	hue_4                 = { "#98c379", 114, "hue_4" }      -- hsl( 95, 38%, 62%);
-	hue_5                 = { "#e06c75", 168, "hue_5" }      -- hsl(355, 65%, 65%);
-	hue_5_2               = { "#be5046", 131, "hue_5_2" }    -- hsl(  5, 48%, 51%);
-	hue_6                 = { "#d19a66", 247, "hue_6" }      -- hsl( 29, 54%, 61%);
-	hue_6_2               = { "#e5c07b", 180, "hue_6_2" }    -- hsl( 39, 67%, 69%);
-	syntax_bg             = { "#282c34", 17, "syntax_bg" }   -- hsl(@syntax-hue, @syntax-saturation, @syntax-brightness);
-	syntax_gutter         = { "#636d83", 241, "syntax_gutter" } -- darken(@syntax-fg, 26%);
-	syntax_cursor         = { "#2c323c", 23, "syntax_cursor" }
-	syntax_accent         = { "#528bff", 69, "syntax_accent" } -- hsl(@syntax-hue, 100%, 66% );
-	vertsplit             = { "#181a1f", 234, "vertsplit" }
-	special_grey          = { "#3b4048", 238, "special_grey" }
-	visual_grey           = { "#3e4452", 59, "visual_grey" }
-	pmenu                 = { "#333841", 59, "pmenu" }
-	term_black            = { "#282c34", 17, "term_black" }
-	term_blue             = { "#61afef", 75, "term_blue" }
-	term_cyan             = { "#56b6c2", 247, "term_cyan" }
-	term_white            = { "#dcdfe4", 188, "term_white" }
-	term_8                = { "#5d677a", 242, "term_8" }
-	syntax_color_added    = { "#43d08a", 78, "syntax_color_added" }  -- hsl(150,  60%, 54%);
-	syntax_color_modified = { "#e0c285", 250, "syntax_color_modified" } -- hsl(40,   60%, 70%);
-	syntax_color_removed  = { "#e05252", 244, "syntax_color_removed" } -- hsl(0,    70%, 60%);
+	mono_1                   = { "#abb2bf", 145, "mono_1" } -- hsl(@syntax-hue, 14%, 71%);
+	mono_2                   = { "#828997", 102, "mono_2" } -- hsl(@syntax-hue,  9%, 55%);
+	mono_3                   = { "#5c6370", 59, "mono_3" }  -- hsl(@syntax-hue, 10%, 40%);
+	mono_4                   = { "#4b5263", 240, "mono_4" } --
+	hue_1                    = { "#56b6c2", 247, "hue_1" }  -- hsl(187, 47%, 55%);
+	hue_2                    = { "#61afef", 75, "hue_2" }   -- hsl(207, 82%, 66%);
+	hue_3                    = { "#c678dd", 176, "hue_3" }  -- hsl(286, 60%, 67%);
+	hue_4                    = { "#98c379", 114, "hue_4" }  -- hsl( 95, 38%, 62%);
+	hue_5                    = { "#e06c75", 168, "hue_5" }  -- hsl(355, 65%, 65%);
+	hue_5_2                  = { "#be5046", 131, "hue_5_2" } -- hsl(  5, 48%, 51%);
+	hue_6                    = { "#d19a66", 247, "hue_6" }  -- hsl( 29, 54%, 61%);
+	hue_6_2                  = { "#e5c07b", 180, "hue_6_2" } -- hsl( 39, 67%, 69%);
+	syntax_bg                = { "#282c34", 17, "syntax_bg" } -- hsl(@syntax-hue, @syntax-saturation, @syntax-brightness);
+	syntax_gutter            = { "#636d83", 241, "syntax_gutter" } -- darken(@syntax-fg, 26%);
+	syntax_cursor            = { "#2c323c", 23, "syntax_cursor" }
+	syntax_accent            = { "#528bff", 69, "syntax_accent" } -- hsl(@syntax-hue, 100%, 66% );
+	vertsplit                = { "#181a1f", 234, "vertsplit" }
+	special_grey             = { "#3b4048", 238, "special_grey" }
+	visual_grey              = { "#3e4452", 59, "visual_grey" }
+	pmenu                    = { "#333841", 59, "pmenu" }
+	term_black               = { "#282c34", 17, "term_black" }
+	term_blue                = { "#61afef", 75, "term_blue" }
+	term_cyan                = { "#56b6c2", 247, "term_cyan" }
+	term_white               = { "#dcdfe4", 188, "term_white" }
+	term_8                   = { "#5d677a", 242, "term_8" }
+	syntax_color_added       = { "#43d08a", 78, "syntax_color_added" } -- hsl(150,  60%, 54%);
+	syntax_color_modified    = { "#e0c285", 250, "syntax_color_modified" } -- hsl(40,   60%, 70%);
+	syntax_color_removed     = { "#e05252", 244, "syntax_color_removed" } -- hsl(0,    70%, 60%);
+	syntax_color_added_bg    = { "#2e8f38", 78, "syntax_color_added_bg" }
+	syntax_color_modified_bg = { "#ce931b", 250, "syntax_color_modified_bg" }
+	syntax_color_removed_bg  = { "#a73636", 244, "syntax_color_removed_bg" }
+	tree_context_bg          = { "#3e444f", 240, "tree_context_bg" }
 else
 	--[[
          Light Colors
@@ -48,34 +52,38 @@ else
          @syntax-gutter-background-color-selected: darken(@syntax-bg, 8%);
          for color in $(cat colors/one-nvim.vim | head -n 79 | tail -n 28 | cut -d '#' -f2 | cut -d '"' -f1); do hex2xterm $color | grep 'Hexadecimal\|xterm256'; done
     --]]
-	mono_1                = { "#383A42", 59, "mono_1" }      -- hsl(@syntax-hue, 8%, 24%);
-	mono_2                = { "#696c77", 60, "mono_2" }      -- hsl(@syntax-hue, 6%, 44%);
-	mono_3                = { "#a0a1a7", 247, "mono_3" }     -- hsl(@syntax-hue, 4%, 64%);
-	mono_4                = { "#c2c2c3", 251, "mono_4" }     --
-	hue_1                 = { "#0184bc", 31, "hue_1" }       -- hsl(198, 99%, 37%);
-	hue_2                 = { "#4078f2", 69, "hue_2" }       -- hsl(221, 87%, 60%);
-	hue_3                 = { "#a626a4", 243, "hue_3" }      -- hsl(301, 63%, 40%);
-	hue_4                 = { "#50a14f", 242, "hue_4" }      -- hsl(119, 34%, 47%);
-	hue_5                 = { "#e45649", 244, "hue_5" }      -- hsl(  5, 74%, 59%);
-	hue_5_2               = { "#ca1243", 241, "hue_5_2" }    -- hsl(344, 84%, 43%);
-	hue_6                 = { "#986801", 94, "hue_6" }       -- hsl(41, 99%, 30%);
-	hue_6_2               = { "#c18401", 242, "hue_6_2" }    -- hsl(41, 99%, 38%)
-	syntax_bg             = { "#fafafa", 231, "syntax_bg" }  -- hsl(@syntax-hue, @syntax-saturation, @syntax-brightness);
-	syntax_gutter         = { "#9e9e9e", 247, "syntax_gutter" } -- darken(@syntax-bg, 36%);
-	syntax_cursor         = { "#f0f0f0", 255, "syntax_cursor" }
-	syntax_accent         = { "#526fff", 246, "syntax_accent" } -- hsl(@syntax-hue, 100%, 66% );
-	vertsplit             = { "#e7e9e1", 254, "vertsplit" }
-	special_grey          = { "#d3d3d3", 252, "special_grey" }
-	visual_grey           = { "#d0d0d0", 252, "visual_grey" }
-	pmenu                 = { "#dfdfdf", 254, "pmenu" }
-	term_black            = { "#383a42", 237, "term_black" }
-	term_blue             = { "#0184bc", 31, "term_blue" }
-	term_cyan             = { "#0997b3", 243, "term_cyan" }
-	term_white            = { "#fafafa", 231, "term_white" }
-	term_8                = { "#4f525e", 240, "term_8" }
-	syntax_color_added    = { "#2db448", 65, "syntax_color_added" }  -- hsl(132,  60%, 44%);
-	syntax_color_modified = { "#f2a60d", 137, "syntax_color_modified" } -- hsl(40,   90%, 50%);
-	syntax_color_removed  = { "#ff1414", 88, "syntax_color_removed" } -- hsl(0,    100%, 54%);
+	mono_1                   = { "#383A42", 59, "mono_1" }  -- hsl(@syntax-hue, 8%, 24%);
+	mono_2                   = { "#696c77", 60, "mono_2" }  -- hsl(@syntax-hue, 6%, 44%);
+	mono_3                   = { "#a0a1a7", 247, "mono_3" } -- hsl(@syntax-hue, 4%, 64%);
+	mono_4                   = { "#c2c2c3", 251, "mono_4" } --
+	hue_1                    = { "#0184bc", 31, "hue_1" }   -- hsl(198, 99%, 37%);
+	hue_2                    = { "#4078f2", 69, "hue_2" }   -- hsl(221, 87%, 60%);
+	hue_3                    = { "#a626a4", 243, "hue_3" }  -- hsl(301, 63%, 40%);
+	hue_4                    = { "#50a14f", 242, "hue_4" }  -- hsl(119, 34%, 47%);
+	hue_5                    = { "#e45649", 244, "hue_5" }  -- hsl(  5, 74%, 59%);
+	hue_5_2                  = { "#ca1243", 241, "hue_5_2" } -- hsl(344, 84%, 43%);
+	hue_6                    = { "#986801", 94, "hue_6" }   -- hsl(41, 99%, 30%);
+	hue_6_2                  = { "#c18401", 242, "hue_6_2" } -- hsl(41, 99%, 38%)
+	syntax_bg                = { "#fafafa", 231, "syntax_bg" } -- hsl(@syntax-hue, @syntax-saturation, @syntax-brightness);
+	syntax_gutter            = { "#9e9e9e", 247, "syntax_gutter" } -- darken(@syntax-bg, 36%);
+	syntax_cursor            = { "#f0f0f0", 255, "syntax_cursor" }
+	syntax_accent            = { "#526fff", 246, "syntax_accent" } -- hsl(@syntax-hue, 100%, 66% );
+	vertsplit                = { "#e7e9e1", 254, "vertsplit" }
+	special_grey             = { "#d3d3d3", 252, "special_grey" }
+	visual_grey              = { "#d0d0d0", 252, "visual_grey" }
+	pmenu                    = { "#dfdfdf", 254, "pmenu" }
+	term_black               = { "#383a42", 237, "term_black" }
+	term_blue                = { "#0184bc", 31, "term_blue" }
+	term_cyan                = { "#0997b3", 243, "term_cyan" }
+	term_white               = { "#fafafa", 231, "term_white" }
+	term_8                   = { "#4f525e", 240, "term_8" }
+	syntax_color_added       = { "#2db448", 65, "syntax_color_added" } -- hsl(132,  60%, 44%);
+	syntax_color_modified    = { "#f2a60d", 137, "syntax_color_modified" } -- hsl(40,   90%, 50%);
+	syntax_color_removed     = { "#ff1414", 88, "syntax_color_removed" } -- hsl(0,    100%, 54%);
+	syntax_color_added_bg    = { "#a1f1ae", 65, "syntax_color_added_bg" }
+	syntax_color_modified_bg = { "#f8d286", 137, "syntax_color_modified_bg" }
+	syntax_color_removed_bg  = { "#ffaeae", 88, "syntax_color_removed_bg" }
+	tree_context_bg          = { "#eeeeee", 240, "tree_context_bg" }
 end
 
 -- Common
@@ -227,12 +235,11 @@ local highlight_groups = {
 	-----------------------
 	-- Diff Highlighting --
 	-----------------------
-	-- FIXME: too bright
-	DiffAdd                              = { fg = syntax_color_added },
-	DiffAdded                            = { fg = syntax_color_added },
-	DiffChange                           = { fg = syntax_color_modified },
-	DiffDelete                           = { fg = syntax_color_removed },
-	DiffRemoved                          = { fg = syntax_color_removed },
+	DiffAdd                              = { bg = syntax_color_added_bg },
+	DiffAdded                            = { bg = syntax_color_added_bg },
+	DiffChange                           = { bg = syntax_color_modified_bg },
+	DiffDelete                           = { bg = syntax_color_removed_bg },
+	DiffRemoved                          = { bg = syntax_color_removed_bg },
 
 	---------------------------
 	-- Filetype Highlighting --
