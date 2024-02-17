90e263a655b98bdc5ea8416fac5fda44fdb56f0a
Léana 江 <leana.jiang@icloud.com>
2024-02-17 13:47:37 +0100

    ref: stylua

--- lua/curry/init.lua
+++ lua/curry/init.lua
@@ -1,18 +1,18 @@
 local M = {}
-local theme = require "curry.theme"
+local theme = require("curry.theme")
 
 M.setup = function()
-  vim.cmd "hi clear"
+	vim.cmd("hi clear")
 
-  vim.o.background = "light"
-  if vim.fn.exists "syntax_on" then
-    vim.cmd "syntax reset"
-  end
+	vim.o.background = "light"
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
 
-  vim.o.termguicolors = true
-  vim.g.colors_name = "curry"
+	vim.o.termguicolors = true
+	vim.g.colors_name = "curry"
 
-  theme.set_highlights()
+	theme.set_highlights()
 end
 
 return M
--- lua/curry/utils.lua
+++
@@ -1,9 +0,0 @@
-M = {}
-
-M.Foreach = function(tbl, f)
-  for k, v in pairs(tbl) do
-    f(k, v)
-  end
-end
-
-return M
